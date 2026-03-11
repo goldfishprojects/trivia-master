1316a6631db389bf2c3fb8e78dffd90d31e23be8
Bart van den Hoven <b.vandenhoven@student.fontys.nl>
2026-03-11 10:08:11 +0100

    Updated CORS config to allow hosted frontend to make requests.

--- api/src/main/java/nl/bartvdhoven/triviamaster/config/CorsConfig.java
+++ api/src/main/java/nl/bartvdhoven/triviamaster/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://goldfishproductions.nl/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
